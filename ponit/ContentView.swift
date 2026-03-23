993cae72b6f254a99e0884a25b01ee2497d731c2
zen-nemuno <nemuno.poni@zen-integration.com>
2026-03-24 01:55:21 +0900

    Initial commit

---
+++ ponit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ponito
+//
+//  Created by 稲田稜 on 2026/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ponit/ponitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ponitoApp.swift
+//  ponito
+//
+//  Created by 稲田稜 on 2026/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ponitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
